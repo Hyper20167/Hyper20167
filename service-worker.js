2dee466effcd4010ed82573bf23c2347b92b9ca9
Hyper20167 <lolopurple9@gmail.com>
2026-07-30 16:02:24 +0300

    Add service worker for caching and fetch handling
    Implement service worker for caching files and handling fetch requests.

---
+++ service-worker.js
@@ -0,0 +1,80 @@
+const CACHE_NAME = "brawl-collect-v1";
+
+const FILES_TO_CACHE = [
+    "index.html",
+    "style.css",
+    "manifest.json",
+    "favicon.png"
+];
+
+
+// INSTALL
+
+self.addEventListener("install", (event) => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+    );
+
+});
+
+
+
+
+// LOAD FROM CACHE
+
+self.addEventListener("fetch", (event) => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then((response) => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
+
+
+
+
+// UPDATE CACHE
+
+self.addEventListener("activate", (event) => {
+
+    event.waitUntil(
+
+        caches.keys()
+
+        .then((cacheNames) => {
+
+            return Promise.all(
+
+                cacheNames.map((cache) => {
+
+                    if(cache !== CACHE_NAME){
+
+                        return caches.delete(cache);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+});
