80ff8bb9f0b95166060530aa487fb0d5b9eb2c77
Hyper20167 <lolopurple9@gmail.com>
2026-07-31 14:49:05 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,80 +1,109 @@
+
 const CACHE_NAME = "brawl-collect-v2";
 
-const FILES_TO_CACHE = [
+
+const filesToCache = [
+
     "index.html",
+
     "style.css",
+
     "manifest.json",
+
     "favicon.png"
+
 ];
 
 
-// INSTALL
 
-self.addEventListener("install", (event) => {
+
+
+self.addEventListener("install", event => {
+
 
     event.waitUntil(
 
         caches.open(CACHE_NAME)
-        .then((cache) => {
 
-            return cache.addAll(FILES_TO_CACHE);
+        .then(cache => {
+
+            return cache.addAll(filesToCache);
 
         })
 
     );
+
 
 });
 
 
 
 
-// LOAD FROM CACHE
-
-self.addEventListener("fetch", (event) => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-
-        .then((response) => {
-
-            return response || fetch(event.request);
-
-        })
-
-    );
-
-});
 
 
 
+self.addEventListener("activate", event => {
 
-// UPDATE CACHE
-
-self.addEventListener("activate", (event) => {
 
     event.waitUntil(
 
         caches.keys()
 
-        .then((cacheNames) => {
+        .then(cacheNames => {
+
 
             return Promise.all(
 
-                cacheNames.map((cache) => {
+                cacheNames.map(cache => {
+
 
                     if(cache !== CACHE_NAME){
 
+
                         return caches.delete(cache);
+
 
                     }
 
+
                 })
 
+
             );
+
 
         })
 
     );
+
+
+});
+
+
+
+
+
+
+
+
+self.addEventListener("fetch", event => {
+
+
+    event.respondWith(
+
+
+        caches.match(event.request)
+
+        .then(response => {
+
+
+            return response || fetch(event.request);
+
+
+        })
+
+
+    );
+
 
 });
